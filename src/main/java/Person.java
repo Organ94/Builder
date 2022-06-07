57190691c85f99b41230fe5ec450145a45222c5a
Алексей Печин <organner@mail.ru>
2022-06-07 16:26:54 +0300

    Creating class Person.java Creating class PersonBuilder.java

---
+++ src/main/java/Person.java
@@ -0,0 +1,66 @@
+public class Person {
+
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String city;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname, int age, String city) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.city = city;
+    }
+
+    public boolean hasAge() {
+        return age != -1;
+    }
+
+    public boolean hasCity() {
+        return city != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(surname)
+                .setAge(0)
+                .setCity(city);
+    }
+}
---
+++ src/main/java/PersonBuilder.java
@@ -0,0 +1,37 @@
+public class PersonBuilder {
+
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected String city;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Не хватает обязательных данных!");
+        }
+        Person person = new Person(name, surname, age);
+        person.setCity(city);
+
+        return person;
+    }
+}
